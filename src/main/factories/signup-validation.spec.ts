7ec369b53bc6224e087503ae954769a60d56da8a
Davison Mota <davimotagnr@gmail.com>
2022-11-19 15:55:23 -0300

    refactor: move password comparation to the ValidationComposite

--- src/main/factories/signup-validation.spec.ts
+++ src/main/factories/signup-validation.spec.ts
@@ -2,6 +2,7 @@ import { makeSignUpValidation } from './signup-validation'
 import { ValidationComposite } from '../../presentation/helpers/validator/validation-composite'
 import { RequiredFieldValidation } from '../../presentation/helpers/validator/required-field-validation'
 import { Validation } from '../../presentation/helpers/validator/validation'
+import { CompareFieldsValidation } from '../../presentation/helpers/validator/compare-field-validation'
 
 jest.mock('../../presentation/helpers/validator/validation-composite')
 
@@ -12,6 +13,7 @@ describe('SignUpValidation Factory', () => {
     for (const field of ['name', 'email', 'password', 'passwordConfirmation']) {
       validations.push(new RequiredFieldValidation(field))
     }
+    validations.push(new CompareFieldsValidation('password', 'passwordConfirmation'))
     expect(ValidationComposite).toHaveBeenCalledWith(validations)
   })
 })
--- src/main/factories/signup-validation.ts
+++ src/main/factories/signup-validation.ts
@@ -1,3 +1,4 @@
+import { CompareFieldsValidation } from '../../presentation/helpers/validator/compare-field-validation'
 import { RequiredFieldValidation } from '../../presentation/helpers/validator/required-field-validation'
 import { Validation } from '../../presentation/helpers/validator/validation'
 import { ValidationComposite } from '../../presentation/helpers/validator/validation-composite'
@@ -7,5 +8,6 @@ export const makeSignUpValidation = (): ValidationComposite => {
   for (const field of ['name', 'email', 'password', 'passwordConfirmation']) {
     validations.push(new RequiredFieldValidation(field))
   }
+  validations.push(new CompareFieldsValidation('password', 'passwordConfirmation'))
   return new ValidationComposite(validations)
 }
