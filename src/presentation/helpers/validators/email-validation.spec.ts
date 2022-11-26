a916c0dfcd05c0958081a49b9b2dde9af7eb81af
Davison Mota <davimotagnr@gmail.com>
2022-11-25 21:12:44 -0300

    feat: add LoginValidationFactory

--- src/presentation/helpers/validators/email-validation.spec.ts
+++ src/presentation/helpers/validators/email-validation.spec.ts
@@ -1,5 +1,5 @@
 import { EmailValidation } from './email-validation'
-import { EmailValidator } from '../../controllers/signup/signup-protocols'
+import { EmailValidator } from '../../protocols/email-validator'
 import { InvalidParamError } from '../../errors'
 
 const makeEmailValidator = (): EmailValidator => {
