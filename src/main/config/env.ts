130b8050d5ec8fa07821f290741e0ab1b8dd60b0
Davison Mota <davimotagnr@gmail.com>
2022-12-02 14:09:52 -0300

    refactor: chenge .env

--- src/main/config/env.ts
+++ src/main/config/env.ts
@@ -1,5 +1,5 @@
 export default {
   mongoUrl: process.env.MONGO_URL || 'mongodb://0.0.0.0:27017/clean-node-api',
   port: process.env.PORT || 5050,
-  jwtSecret: process.env.JWT_SECRET || 'am-+@vp*1b3)25541ÇVBÇ~PRFGÇmcogw21n>'
+  jwtSecret: process.env.JWT_SECRET || 'IIe5ncPDlESIGXXavo12b'
 }
