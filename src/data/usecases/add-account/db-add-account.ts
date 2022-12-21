924a303d44af39790991b3ecee81610f66477929
Davison Mota <davimotagnr@gmail.com>
2022-12-21 18:23:54 -0300

    feat: ensure DbAddAccount retunrs null if LoadAccountByEmailRepositoryreturns an account
    Signed-off-by: Davison Mota <davimotagnr@gmail.com>

--- src/data/usecases/add-account/db-add-account.ts
+++ src/data/usecases/add-account/db-add-account.ts
@@ -8,7 +8,8 @@ export class DbAddAccount implements AddAccount {
   ) {}
 
   async add (accountData: AddAccountModel): Promise<AccountModel> {
-    await this.loadAccountByEmailRepository.loadByEmail(accountData.email)
+    const accontExists = await this.loadAccountByEmailRepository.loadByEmail(accountData.email)
+    if (accontExists) return null
     const hashedPassword = await this.hasher.hash(accountData.password)
     const account = await this.addAccountRepository.add(Object.assign({}, accountData, { password: hashedPassword }))
     return account
