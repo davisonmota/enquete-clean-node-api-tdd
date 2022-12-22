32613bc6431a9ac888685f277bcceb2fa15b64bd
Davison Mota <davimotagnr@gmail.com>
2022-12-22 10:40:35 -0300

    docs: update requirements
    Signed-off-by: Davison Mota <davimotagnr@gmail.com>

--- src/data/usecases/add-account/db-add-account.ts
+++ src/data/usecases/add-account/db-add-account.ts
@@ -8,8 +8,8 @@ export class DbAddAccount implements AddAccount {
   ) {}
 
   async add (accountData: AddAccountModel): Promise<AccountModel> {
-    const accontExists = await this.loadAccountByEmailRepository.loadByEmail(accountData.email)
-    if (accontExists) return null
+    const accountExists = await this.loadAccountByEmailRepository.loadByEmail(accountData.email)
+    if (accountExists) return null
     const hashedPassword = await this.hasher.hash(accountData.password)
     const account = await this.addAccountRepository.add(Object.assign({}, accountData, { password: hashedPassword }))
     return account
