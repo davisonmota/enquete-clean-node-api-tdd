1fd1e223892a56dc738170c615b5ec4afea0cc26
Davison Mota <davimotagnr@gmail.com>
2022-12-26 18:04:26 -0300

    test: ensure AccountMongoRepository find an account by token with role
    Signed-off-by: Davison Mota <davimotagnr@gmail.com>

--- src/infra/db/mongodb/account/account-mongo-repository.spec.ts
+++ src/infra/db/mongodb/account/account-mongo-repository.spec.ts
@@ -95,5 +95,22 @@ describe('Account Mongo Repository', () => {
       expect(account.email).toBe('any_mail@mail.com')
       expect(account.password).toBe('hashed_password')
     })
+
+    test('Should return an account on loadByToken with role', async () => {
+      const sut = makeSut()
+      await accountCollection.insertOne({
+        name: 'any_name',
+        email: 'any_mail@mail.com',
+        password: 'hashed_password',
+        accessToken: 'any_token',
+        role: 'any_role'
+      })
+      const account = await sut.loadByToken('any_token', 'any_role')
+      expect(account).toBeTruthy()
+      expect(account.id).toBeTruthy()
+      expect(account.name).toBe('any_name')
+      expect(account.email).toBe('any_mail@mail.com')
+      expect(account.password).toBe('hashed_password')
+    })
   })
 })
