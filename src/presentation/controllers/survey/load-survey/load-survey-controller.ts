2f16d6d1c163f2841c0ca939d294f50bf24cb860
Davison Mota <davimotagnr@gmail.com>
2022-12-27 15:22:31 -0300

    feat: ensure LoadSurveysController returns 500 if LoadSurveysUseCase throws
    Signed-off-by: Davison Mota <davimotagnr@gmail.com>

--- src/presentation/controllers/survey/load-survey/load-survey-controller.ts
+++ src/presentation/controllers/survey/load-survey/load-survey-controller.ts
@@ -1,12 +1,16 @@
 import { Controller, HttpRequest, HttpResponse, LoadSurveys } from './load-survey-controller-protocols'
-import { ok } from '../../../helpers/http/http-helper'
+import { ok, serverError } from '../../../helpers/http/http-helper'
 export class LoadSurveyController implements Controller {
   constructor (
     private readonly loadSurvey: LoadSurveys
   ) {}
 
   async handle (httpRequest: HttpRequest): Promise<HttpResponse> {
-    const surveys = await this.loadSurvey.load()
-    return ok(surveys)
+    try {
+      const surveys = await this.loadSurvey.load()
+      return ok(surveys)
+    } catch (error) {
+      return serverError(error)
+    }
   }
 }
