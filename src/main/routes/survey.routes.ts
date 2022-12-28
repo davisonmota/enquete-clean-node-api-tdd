0b2e680f785916ef52897100a8d5044f3ecd021f
Davison Mota <davimotagnr@gmail.com>
2022-12-28 14:49:01 -0300

    feat: ensure load surveys 403 if no accesToken is provided
    Signed-off-by: Davison Mota <davimotagnr@gmail.com>

--- src/main/routes/survey.routes.ts
+++ src/main/routes/survey.routes.ts
@@ -2,9 +2,13 @@ import { Router } from 'express'
 import { adaptMiddleware } from '../adapters/express/express-middleware-adapter'
 import { adaptRoute } from '../adapters/express/express-route-adapter'
 import { makeAddSurveyController } from '../factories/controllers/survey/add-survey/add-survey-controller-factory'
+import { makeLoadSurveysController } from '../factories/controllers/survey/load-surveys/load-survey-controller-factory'
 import { makeAuthMiddleware } from '../factories/middlewares/auth-middleware-controller-factory'
 
 export default (router: Router): void => {
   const adminAuth = adaptMiddleware(makeAuthMiddleware('admin'))
   router.post('/surveys', adminAuth, adaptRoute(makeAddSurveyController()))
+
+  const auth = adaptMiddleware(makeAuthMiddleware())
+  router.get('/surveys', auth, adaptRoute(makeLoadSurveysController()))
 }
