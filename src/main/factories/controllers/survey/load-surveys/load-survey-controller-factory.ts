3e512ae04550e4d74d4ef40e5ce598d07bf9c7f9
Davison Mota <davimotagnr@gmail.com>
2023-01-04 15:37:05 -0300

    feat: ensure save survey result route returns 403 if no token is provided

--- src/main/factories/controllers/survey/load-surveys/load-survey-controller-factory.ts
+++ src/main/factories/controllers/survey/load-surveys/load-survey-controller-factory.ts
@@ -1,5 +1,5 @@
 import { makeLogControllerDecorator } from '@/main/factories/decorators/log-controller-decorator-factory'
-import { makeDbLoadSurveys } from '@/main/factories/usecases/survey/load-surveys/db-load-surveys'
+import { makeDbLoadSurveys } from '@/main/factories/usecases/survey/load-surveys/db-load-surveys-factory'
 import { LoadSurveyController } from '@/presentation/controllers/survey/load-survey/load-survey-controller'
 import { Controller } from '@/presentation/protocols'
 
