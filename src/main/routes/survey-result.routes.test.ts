7cdbb4b6be2ceb53a22938c98b310ca5681d2e75
Davison Mota <davimotagnr@gmail.com>
2023-01-04 16:19:06 -0300

    test: ensure save survey result route returns 200 on success

--- src/main/routes/survey-result.routes.test.ts
+++ src/main/routes/survey-result.routes.test.ts
@@ -1,7 +1,27 @@
 import app from '@/main/config/app'
+import env from '@/main/config/env'
 import { MongoHelper } from '@/infra/db/mongodb/helpers/mongo-helper'
+import { sign } from 'jsonwebtoken'
 import { Collection } from 'mongodb'
 import request from 'supertest'
+
+const makeAccessToken = async (): Promise<string> => {
+  const res = await accountCollection.insertOne({
+    name: 'Davison',
+    email: 'davison@gmail.com',
+    password: '123'
+  })
+  const id = res.insertedId
+  const accessToken = sign({ id }, env.jwtSecret)
+  await accountCollection.updateOne({
+    _id: id
+  }, {
+    $set: {
+      accessToken
+    }
+  })
+  return accessToken
+}
 
 let surveyCollection: Collection
 let accountCollection: Collection
@@ -23,13 +43,35 @@ describe('Survey Routes', () => {
   })
 
   describe('PUT /surveys/:surveyId/results', () => {
-    test('Should return 403 on save survey retults without accessToken', async () => {
+    test('Should return 403 on save survey result without accessToken', async () => {
       await request(app)
         .put('/api/surveys/any_id/results')
         .send({
           answer: 'any_answer'
         })
         .expect(403)
+    })
+
+    test('Should return 4200 on save survey result with accessToken', async () => {
+      const accessToken = await makeAccessToken()
+      const res = await surveyCollection.insertOne({
+        question: 'Question',
+        answers: [{
+          image: 'http://image-name.com',
+          answer: 'Answer 1'
+        }, {
+          answer: 'Answer 2'
+        }],
+        date: new Date()
+      })
+      const id = res.insertedId.toHexString()
+      await request(app)
+        .put(`/api/surveys/${id}/results`)
+        .set('x-access-token', accessToken)
+        .send({
+          answer: 'Answer 1'
+        })
+        .expect(200)
     })
   })
 })
