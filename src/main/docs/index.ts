cf1a3811ded8c8e57d96e162c39d29a09ae50a87
Davison Mota <davimotagnr@gmail.com>
2023-01-06 16:28:50 -0300

    docs: add login api docs

--- src/main/docs/index.ts
+++ src/main/docs/index.ts
@@ -1,8 +1,27 @@
+import { loginPath } from './paths/login-paths'
+import { accountSchema } from './schemas/account-schema'
+import { loginParamsSchema } from './schemas/login-params-schema'
+
 export default {
   openapi: '3.0.0',
   info: {
     title: 'Clean Node API',
     description: 'API desenvolvida em NodeJs usando Typescript, TDD, Clean Architecture, Design Patterns e SOLID principles.',
     version: '1.0.0'
+  },
+  servers: [{
+    url: '/api'
+
+  }],
+  tags: [{
+    name: 'Login'
+  }],
+  paths: {
+    '/login': loginPath
+  },
+  schemas: {
+    account: accountSchema,
+    loginParams: loginParamsSchema
+
   }
 }
