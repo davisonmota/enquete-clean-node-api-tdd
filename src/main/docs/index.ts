c7c3f1a65a52e70e529ce37bb12f67f438a0ce0a
Davison Mota <davimotagnr@gmail.com>
2023-01-06 13:41:42 -0300

    feat: add swagger api

---
+++ src/main/docs/index.ts
@@ -0,0 +1,8 @@
+export default {
+  openapi: '3.0.0',
+  info: {
+    title: 'Clean Node API',
+    description: 'API desenvolvida em NodeJs usando Typescript, TDD, Clean Architecture, Design Patterns e SOLID principles.',
+    version: '1.0.0'
+  }
+}
